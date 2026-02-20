bdac2c5f7650e5dbadefcfed0c68bb715f93374f
abcdef_Z <139262330+tarokhalder@users.noreply.github.com>
2026-02-20 15:49:20 +0600

    Refactor DSU constructor and methods for clarity

--- Disjoint Set Union/Union By Rank and Path Compression .cpp
+++ Disjoint Set Union/Union By Rank and Path Compression .cpp
@@ -7,25 +7,25 @@ class dsu{
       vector <int> Parent, Rank;
       dsu(int n) {
           Parent.resize(n + 1);
-	  Rank.resize(n + 1 , 1);
-	  iota(Parent.begin() , Parent.end() , 0);
+	      Rank.resize(n + 1 , 1);
+	      iota(Parent.begin() , Parent.end() , 0);
       }
       int find(int x) {
           if (x == Parent[x]) {
-	     return x;
-	  }
-	  return Parent[x] = find(Parent[x]);
+	         return x;
+	      }
+	     return Parent[x] = find(Parent[x]);
       }
       void unite(int x , int y) {
            x = find(x);
-	   y = find(y);
-	   if (x != y) {
-	      if (Rank[x] < Rank[y]) {
-	         swap(x , y);
+	       y = find(y);
+	      if (x != y) {
+	         if (Rank[x] < Rank[y]) {
+	           swap(x , y);
+	         }
+	         Parent[y] = x;
+	         Rank[x] += Rank[y];
 	      }
-	      Parent[y] = x;
-	      Rank[x] += Rank[y];
-	   }
       }
 };
 
