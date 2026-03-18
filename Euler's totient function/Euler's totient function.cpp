557101f45f113f0262cff45c795577f816cdb87c
abcdef_Z <139262330+tarokhalder@users.noreply.github.com>
2026-03-18 10:38:10 +0600

    Create Euler's totient function.cpp

---
+++ Euler's totient function/Euler's totient function.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int phi(int n) {
+    int Ans = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+	         while (n % i == 0) {
+	            n /= i;
+	         }
+	         Ans -= (Ans / i);
+	        }
+        }
+        if (n > 1) {
+          Ans -= (Ans / n);
+        }
+    return Ans;
+}
+
+int main() {
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        int n;
+	      cin >> n;
+	      cout << phi(n) << "\n";
+    }
+    return 0;
+}
