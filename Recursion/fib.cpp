7a7e689ae11416abd8b40732addea9a29cedf446
abcdef_Z <139262330+tarokhalder@users.noreply.github.com>
2026-05-20 16:17:38 +0600

    Add Fibonacci recursive function implementation

---
+++ Recursion/fib.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fib(int n) {
+    if (n <= 1) {
+       return n;
+    }
+    int f1 = fib(n - 1);
+    int f2 = fib(n - 2);
+    return f1 + f2;
+}
+
+int main() {
+     int n;
+     cin >> n;
+     cout << fib(n) << "\n";
+     return 0;
+}
